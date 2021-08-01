9bd72398ee384d9fcaf4291ae563bd786542b7d9
Daniel Granada <j.daniel_granada@hotmail.com>
2021-08-01 17:24:20 -0500

    testing code

--- Hangman_game.py
+++ Hangman_game.py
@@ -1,39 +1,11 @@
 import os
 import random
 
-def star_menu():
-    flag_a = True
-
-    while flag_a:
-        os.system("clear")
-        opcion = int(input("""
- Do you want to exit?
-
-    1 for Yes
-    2 for No
-
- Please, select a number:"""))
-        if opcion == 1:
-            flag_a = False    
-        else:
-            flag_a = True
-            
-    
-    print("\n","thanks")
-
-
-def read():
+def choosing_word():
     words = []
     with open("./Files/words.txt", "r") as f:
         for line in f:
-            words.append(line.strip())
-    random_word = random.choice(words)
-    print(random_word)
-
-
-def run():
-    read()
-      
-
-if __name__=='__main__':
-    run()
+            words.append(line.strip().upper())
+    
+    chosen_word = random.choice(words)
+    print(chosen_word)
