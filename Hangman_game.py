273128d48d561a8295c5086212f10205669650c6
Daniel Granada <j.daniel_granada@hotmail.com>
2021-08-01 18:30:53 -0500

    Code base

--- Hangman_game.py
+++ Hangman_game.py
@@ -6,6 +6,14 @@ def choosing_word():
     with open("./Files/words.txt", "r") as f:
         for line in f:
             words.append(line.strip().upper())
-    
+
     chosen_word = random.choice(words)
-    print(chosen_word)
+    censored_word = '_ ' * len(chosen_word)
+
+    return chosen_word, censored_word
+
+
+chosen_word, censoring_word = choosing_word()
+
+print(chosen_word)
+print(censoring_word)
