806b6102512b42a4ab616288624bf258f334571b
Daniel Granada <j.daniel_granada@hotmail.com>
2021-08-01 22:04:51 -0500

    testing code

--- Hangman_game.py
+++ Hangman_game.py
@@ -1,19 +1,56 @@
 import os
 import random
 
-def choosing_word():
-    words = []
-    with open("./Files/words.txt", "r") as f:
-        for line in f:
-            words.append(line.strip().upper())
+# flag = True
 
-    chosen_word = random.choice(words)
-    censored_word = '_ ' * len(chosen_word)
+my_test_word = 'celphone'
+test_list = [letter for letter in my_test_word]
+index_list = [(i,l) for i, l in enumerate(test_list)]
+index_dict = {}
 
-    return chosen_word, censored_word
+for idx, letter in enumerate(my_test_word):
+        if not index_dict.get(letter): 
+            index_dict[letter] = []
+        index_dict[letter].append(idx)
+
+# chosen_word = input('chose one letter: ')
+
+# find_letter = my_test_word.find(chosen_word)
+
+print(index_dict)
 
 
-chosen_word, censoring_word = choosing_word()
+# def main_menu():
+#     print(f"""
+# Welcome.
 
-print(chosen_word)
-print(censoring_word)
+# the word is {my_test_word}
+# """)
+
+
+# while flag:
+#     os.system('clear')
+#     main_menu()
+#     chosen_letter = str(input('choice one letter or number 1 to exit: ')).upper()
+#     var = len(chosen_letter)
+
+#     if var >= 0:
+#         if chosen_letter == '1':
+#             os.system('clear')
+#             print('')
+#             print('Thanks')
+#             print('')
+#             flag = False
+        
+#         else:
+#             os.system('clear')
+#             print('')
+#             print(f'The chosen letter was {chosen_letter}')
+#             print('')
+#             choice = input('type Enter key')
+#     else:
+#         os.system('clear')
+#         print('empy')
+#         print('')
+#         choice = input('type Enter key')
+    
