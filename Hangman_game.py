e58f40d5ccbf821d152b608ccb8e01331d87d48c
Daniel Granada <j.daniel_granada@hotmail.com>
2021-09-02 20:10:05 -0500

    testing code

--- Hangman_game.py
+++ Hangman_game.py
@@ -1,21 +1,23 @@
 import os
 import random
 
-# flag = True
+words = []
 
-my_test_word = 'celphone'.upper()
-letters_of_test_word = [letter for letter in my_test_word]
-hidden_word = ['_'] * len(my_test_word)
+with open('./Files/words.txt', 'r', encoding="utf-8") as f:
+    for l in f:
+        words.append(l.strip().replace(" ","").upper())
+
+random_word = random.choice(words)
+
+letters_of_test_word = [letter for letter in random_word]
+hidden_word = ['_'] * len(random_word)
 index_dict = {}
 
-for idx, letter in enumerate(my_test_word):
+for idx, letter in enumerate(random_word):
         if not index_dict.get(letter): 
             index_dict[letter] = []
         index_dict[letter].append(idx)
 
-# chosen_word = input('chose one letter: ')
-
-# find_letter = my_test_word.find(chosen_word)
 
 print(index_dict)
 while True:
@@ -33,39 +35,3 @@ while True:
     if '_' not in hidden_word:
         print('You win')
         break
-
-
-# def main_menu():
-#     print(f"""
-# Welcome.
-
-# the word is {my_test_word}
-# """)
-
-
-# while flag:
-#     os.system('clear')
-#     main_menu()
-#     chosen_letter = str(input('choice one letter or number 1 to exit: ')).upper()
-#     var = len(chosen_letter)
-
-#     if var >= 0:
-#         if chosen_letter == '1':
-#             os.system('clear')
-#             print('')
-#             print('Thanks')
-#             print('')
-#             flag = False
-        
-#         else:
-#             os.system('clear')
-#             print('')
-#             print(f'The chosen letter was {chosen_letter}')
-#             print('')
-#             choice = input('type Enter key')
-#     else:
-#         os.system('clear')
-#         print('empy')
-#         print('')
-#         choice = input('type Enter key')
-    
