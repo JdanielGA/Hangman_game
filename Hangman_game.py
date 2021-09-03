c5cfae84921aab6904d0696a3ee1357e02aacedb
Daniel Granada <j.daniel_granada@hotmail.com>
2021-09-02 20:43:46 -0500

    new file

--- Hangman_game.py
+++ Hangman_game.py
@@ -1,37 +1,54 @@
 import os
 import random
 
-words = []
-
-with open('./Files/words.txt', 'r', encoding="utf-8") as f:
-    for l in f:
-        words.append(l.strip().replace(" ","").upper())
-
-random_word = random.choice(words)
-
-letters_of_test_word = [letter for letter in random_word]
-hidden_word = ['_'] * len(random_word)
-index_dict = {}
-
-for idx, letter in enumerate(random_word):
-        if not index_dict.get(letter): 
-            index_dict[letter] = []
-        index_dict[letter].append(idx)
 
 
-print(index_dict)
-while True:
-    for item in hidden_word:
-        print(item, " ", end="")
-    print("\n")
+def read(data_packet='./Files'):
 
-    letter = input("Enter a letter: ").strip().upper()
-    print(f'The letter chossen was {letter}')
+    word = []
+    with open(data_packet, 'r', encoding="utf-8") as f:
+        for l in f:
+            word.append(l.strip().replace(" ","").upper())
+    return word
 
-    if letter in letters_of_test_word:
-        for idx in index_dict[letter]:
-            hidden_word[idx] = letter
+def run():
+
+    choose = int(input("choose an option: 1-test or 2-words: "))
+
+    if choose == 1:
+        data = read(data_packet='./Files/test_words.txt')
+        
     
-    if '_' not in hidden_word:
-        print('You win')
-        break
+    elif choose == 2:
+        data = read(data_packet='./Files/words.txt')
+
+    random_word = random.choice(data)
+
+    letters_of_test_word = [letter for letter in random_word]
+    hidden_word = ['_'] * len(random_word)
+    index_dict = {}
+
+    for idx, letter in enumerate(random_word):
+            if not index_dict.get(letter): 
+                index_dict[letter] = []
+            index_dict[letter].append(idx)
+
+    print(index_dict)
+    while True:
+        for item in hidden_word:
+            print(item, " ", end="")
+        print("\n")
+
+        letter = input("Enter a letter: ").strip().upper()
+        print(f'The letter chossen was {letter}')
+
+        if letter in letters_of_test_word:
+            for idx in index_dict[letter]:
+                hidden_word[idx] = letter
+        
+        if '_' not in hidden_word:
+            print('You win')
+            break
+
+if __name__ == '__main__':
+    run()
