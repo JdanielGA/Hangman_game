0408301d0b46d28914a41ccf25fb6f38d4d0cf4e
Daniel Granada <j.daniel_granada@hotmail.com>
2021-09-04 19:01:51 -0500

    implementation of lives with the start menu

--- Hangman_game.py
+++ Hangman_game.py
@@ -1,6 +1,34 @@
 import os
 import random
 
+menu = """
+
+                                    _                              _         
+                    __ __ __  ___  | |  __   ___   _ __    ___    | |_   ___ 
+                    \ V  V / / -_) | | / _| / _ \ | '  \  / -_)   |  _| / _ \ 
+                     \_/\_/  \___| |_| \__| \___/ |_|_|_| \___|    \__| \___/
+                    
+  _   _                                                          ____                            
+ | | | |   __ _   _ __     __ _   _ __ ___     __ _   _ __      / ___|   __ _   _ __ ___     ___ 
+ | |_| |  / _` | | '_ \   / _` | | '_ ` _ \   / _` | | '_ \    | |  _   / _` | | '_ ` _ \   / _ \ 
+ |  _  | | (_| | | | | | | (_| | | | | | | | | (_| | | | | |   | |_| | | (_| | | | | | | | |  __/
+ |_| |_|  \__,_| |_| |_|  \__, | |_| |_| |_|  \__,_| |_| |_|    \____|  \__,_| |_| |_| |_|  \___|
+                          |___/  
+
+
+                                              +---+
+                                              |   |
+                                              ¨   |
+                                                  |
+                                                  |
+                                            ¯¯¯¯¯ |
+                                            ============
+
+                                        Do you want to play?
+                                        1 - Yes.
+                                        2 - No.
+"""
+
 hangman_pick = ["""
                                               +---+
                                               |   |
@@ -67,6 +95,27 @@ hangman_pick = ["""
                                             ============
 """]
 
+game_optios = """
+                                    _                              _         
+                    __ __ __  ___  | |  __   ___   _ __    ___    | |_   ___ 
+                    \ V  V / / -_) | | / _| / _ \ | '  \  / -_)   |  _| / _ \ 
+                     \_/\_/  \___| |_| \__| \___/ |_|_|_| \___|    \__| \___/
+                    
+  _   _                                                          ____                            
+ | | | |   __ _   _ __     __ _   _ __ ___     __ _   _ __      / ___|   __ _   _ __ ___     ___ 
+ | |_| |  / _` | | '_ \   / _` | | '_ ` _ \   / _` | | '_ \    | |  _   / _` | | '_ ` _ \   / _ \ 
+ |  _  | | (_| | | | | | | (_| | | | | | | | | (_| | | | | |   | |_| | | (_| | | | | | | | |  __/
+ |_| |_|  \__,_| |_| |_|  \__, | |_| |_| |_|  \__,_| |_| |_|    \____|  \__,_| |_| |_| |_|  \___|
+                          |___/  
+
+                                        GAME OPTIONS
+
+                                    1 - Parts of a house
+                                    2 - Test
+                                    0 - Exit
+
+"""
+
 
 def read(data_packet):
 
@@ -78,59 +127,68 @@ def read(data_packet):
 
 def run():
 
-    choose = int(input("choose an option: 1-test or 2-words: "))
-
-    if choose == 1:
-        data = read(data_packet='./Files/test_words.txt')
-        
-    
-    elif choose == 2:
-        data = read(data_packet='./Files/words.txt')
-
-    random_word = random.choice(data)
-
-    letters_of_test_word = [letter for letter in random_word]
-    hidden_word = ['_'] * len(random_word)
-    index_dict = {}
-
-    for idx, letter in enumerate(random_word):
-            if not index_dict.get(letter): 
-                index_dict[letter] = []
-            index_dict[letter].append(idx)
-
     lives = 7
     position = 0
 
-    while True:
+    os.system('clear')
+    print(menu)
+    play_election = int(input("Choose an option: "))
+
+    if play_election == 1:
 
         os.system('clear')
-        print(f'you have {lives} lives')
-        print(hangman_pick[position])
-
-        for item in hidden_word:
-            print(item, " ", end="")
-        print("\n")
-
-        letter = input("Enter a letter: ").strip().upper()
-        print(f'The letter chossen was {letter}')
-
-        if letter in letters_of_test_word:
-            for idx in index_dict[letter]:
-                hidden_word[idx] = letter
+        print(game_optios)
+        choose = int(input('Choose an option: '))
         
-        else:
-            lives -= 1
-            position += 1
- 
-        if '_' not in hidden_word:
-            os.system('clear')
-            print(f'You win the word was {random_word}')
-            break
+        if choose == 1:
+            data = read(data_packet='./Files/words.txt')
 
-        if lives == 0:
+        if choose == 2:
+            data = read(data_packet='./Files/test_words.txt')
+            
+        
+
+        random_word = random.choice(data)
+
+        letters_of_test_word = [letter for letter in random_word]
+        hidden_word = ['_'] * len(random_word)
+        index_dict = {}
+
+        for idx, letter in enumerate(random_word):
+                if not index_dict.get(letter): 
+                    index_dict[letter] = []
+                index_dict[letter].append(idx)
+
+        while True:
+
             os.system('clear')
-            print(f'You lost, the word waf {random_word}')
-            break
+            print(f'you have {lives} lives')
+            print(hangman_pick[position])
+
+            for item in hidden_word:
+                print(item, " ", end="")
+            print("\n")
+
+            letter = input("Enter a letter: ").strip().upper()
+            print(f'The letter chossen was {letter}')
+
+            if letter in letters_of_test_word:
+                for idx in index_dict[letter]:
+                    hidden_word[idx] = letter
+            
+            else:
+                lives -= 1
+                position += 1
+    
+            if '_' not in hidden_word:
+                os.system('clear')
+                print(f'You win the word was {random_word}')
+                break
+
+            if lives == 0:
+                os.system('clear')
+                print(f'You lost, the word waf {random_word}')
+                break
 
 if __name__ == '__main__':
     run()
